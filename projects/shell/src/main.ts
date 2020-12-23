2519676d09c43b5e02cdfa0f40e87df9159af401
vlatkoMiloshevski <vlatko.miloshevski@gmail.com>
2020-12-23 20:45:47 +0100

    firebase & prod setup

--- projects/shell/src/main.ts
+++ projects/shell/src/main.ts
@@ -1,9 +1,21 @@
 import { loadRemoteEntry } from '@angular-architects/module-federation';
+import { environment } from './environments/environment';
 
-Promise.all([
-    loadRemoteEntry('http://localhost:3000/remoteEntry.js', 'app1'),
-    loadRemoteEntry('http://localhost:3100/remoteEntry.js', 'app2')
-])
-.catch(err => console.error('Error loading remote entries', err))
-.then(() => import('./bootstrap'))
-.catch(err => console.error(err));
+let entries = [];
+// ToDo: register local/remote microfrontend apps
+if (environment.production) {
+    entries.push([
+        loadRemoteEntry('https://micofontend-1.web.app/remoteEntry.js', 'app1'),
+        loadRemoteEntry('https://microfrontend-2.web.app/remoteEntry.js', 'app2')
+    ]);
+} else {
+    entries = [
+        loadRemoteEntry('http://localhost:3000/remoteEntry.js', 'app1'),
+        loadRemoteEntry('http://localhost:3200/remoteEntry.js', 'app2')
+    ];
+}
+
+Promise.all(entries)
+    .catch(err => console.error('Error loading remote entries', err))
+    .then(() => import('./bootstrap'))
+    .catch(err => console.error(err));
