ea034b756b2137744819af2ea573afea3dcef27b
vlatko <vlatko.miloshevski@gmail.com>
2020-12-23 20:48:36 +0100

    change port number

--- projects/shell/src/main.ts
+++ projects/shell/src/main.ts
@@ -11,7 +11,7 @@ if (environment.production) {
 } else {
     entries = [
         loadRemoteEntry('http://localhost:3000/remoteEntry.js', 'app1'),
-        loadRemoteEntry('http://localhost:3200/remoteEntry.js', 'app2')
+        loadRemoteEntry('http://localhost:3100/remoteEntry.js', 'app2')
     ];
 }
 
